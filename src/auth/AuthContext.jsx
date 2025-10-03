29e72547429c77ef941261735b2a06e3c1018f62
SHussain47 <hussain.syed.6732@gmail.com>
2025-10-02 21:59:12 -0400

    feat: Basic template for AuthContext (AuthProvider,useAuth) | Added token

---
+++ src/auth/AuthContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useContext, useState } from "react";
+
+const API = import.meta.env.API;
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [token, setToken] = useState();
+
+  const value = { token };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if(!context) throw Error("useAuth can only be used within AuthProvider");
+  return context;
+}
