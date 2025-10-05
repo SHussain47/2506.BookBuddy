4c8c5c53a300914e93f771ebe01657c033fe824b
SHussain47 <hussain.syed.6732@gmail.com>
2025-10-04 22:30:27 -0400

    feat: Added register logic and register.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { Routes, Route } from "react-router";
+import Register from "./auth/Register";
+
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Test</h1>
+      <Register />
+    </>
+  )
 }
