07327fbe7145b6d3119beb51ed044aa69d7ca5df
isabelleadrury <isabelledrury38@gmail.com>
2025-10-13 18:18:18 -0400

    added return-action

--- src/account/Account.jsx
+++ src/account/Account.jsx
@@ -3,14 +3,28 @@ import { getMyReservations } from "../api/reservations";
 
 export default function Account() {
   const [reservations, setReservations] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchReservations() {
       const data = await getMyReservations();
       setReservations(data);
+      setLoading(false);
     }
     fetchReservations();
   }, []);
+
+  async function handleReturn(reservationId) {
+    const success = await returnBook(reservationId);
+    if (success) {
+      alert("Return book completed!");
+      const result = await getMyReservations();
+      setReservations(result);
+    }
+  }
+
+  if (loading) return <p>Loading...</p>;
+
   return (
     <article>
       <h2>My Reservations</h2>
@@ -24,6 +38,9 @@ export default function Account() {
               <div>
                 <h3>{book.title}</h3>
                 <p>{book.author}</p>
+                <button onClick={() => handleReturn(book.id)}>
+                  Return Book
+                </button>
               </div>
             </div>
           ))}
